dfa02e8babf59056a7a8cf199e35e25abe7fc260
ubungmeister <ubungmeister@gmail.com>
2025-06-04 18:27:31 +0200

    fix/security config for deploy

--- backend/src/main/java/com/trainerlog/security/SecurityConfig.java
+++ backend/src/main/java/com/trainerlog/security/SecurityConfig.java
@@ -2,35 +2,22 @@ package com.trainerlog.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.method.P;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.trainerlog.model.user.User;
 
 @Configuration
 @EnableWebSecurity
-
 public class SecurityConfig {
-  @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-  @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() 
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll() 
-                .anyRequest().authenticated()                
+                .anyRequest().permitAll()
             );
-
+        
         return http.build();
     }
-
 }
