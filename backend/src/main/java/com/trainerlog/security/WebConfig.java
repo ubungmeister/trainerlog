4f5be176f9c7df2fb44ea2968ef9334bcf943302
ubungmeister <ubungmeister@gmail.com>
2025-06-07 11:53:09 +0200

    fix cors

--- backend/src/main/java/com/trainerlog/security/WebConfig.java
+++ backend/src/main/java/com/trainerlog/security/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173"  )
+                    .allowedOrigins(ALLOWED_ORIGINS)
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
