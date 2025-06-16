ec284d46e0274190b938675cefb471d0abc2bad7
ubungmeister <ubungmeister@gmail.com>
2025-06-16 14:34:53 +0200

    fix/fetch req

--- frontend/src/services/auth/useRegister.tsx
+++ frontend/src/services/auth/useRegister.tsx
@@ -1,25 +1,24 @@
-
 import { useMutation } from "@tanstack/react-query";
 import type { SignupFormData } from "../../pages/auth/SignUp";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const useRegister = () => {
-    return useMutation({
-        mutationFn: async (data: SignupFormData ) => {
-            const response = await fetch(`${API_URL}/api/auth/register`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            });
-
-            if (!response.ok) {
-                throw new Error("Registration failed");
-            }
-
-            return response.json();
+  return useMutation({
+    mutationFn: async (data: SignupFormData) => {
+      const response = await fetch(`${API_URL}/api/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-    });
+        body: JSON.stringify(data),
+        credentials: "include",
+      });
 
-}
+      if (!response.ok) {
+        throw new Error("Registration failed");
+      }
+
+      return response.json();
+    },
+  });
+};
